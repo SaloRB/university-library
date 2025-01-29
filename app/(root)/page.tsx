dedf902e3d6249652c29f92db0763ced5bec2187
Salomon Romano <salo_romano@yahoo.com>
2025-01-29 16:48:24 -0600

    Add BookVideo component and implement video handling; update Home and Book pages to display video; add database seeding script for initial data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from '@/auth'
 import BookList from '@/components/BookList'
 import BookOverview from '@/components/BookOverview'
 import { db } from '@/database/drizzle'
@@ -5,6 +6,8 @@ import { books } from '@/database/schema'
 import { desc } from 'drizzle-orm'
 
 const Home = async () => {
+  const session = await auth()
+
   const latestBooks = (await db
     .select()
     .from(books)
@@ -13,10 +16,10 @@ const Home = async () => {
 
   return (
     <>
-      <BookOverview {...latestBooks[0]} />
+      <BookOverview {...latestBooks[0]} userId={session?.user?.id as string} />
       <BookList
         title="Latest Books"
-        books={latestBooks}
+        books={latestBooks.slice(1)}
         containerClassName="mt-28"
       />
     </>
